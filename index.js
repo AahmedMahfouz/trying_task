3a1c83c8d14bff458db75e3b83457fad9dcf0088
Ahmed Mahfouz <ahmedmahfouz3032004@gmail.com>
2026-05-04 06:13:50 +0300

    Final fix: ensure sync and root route

--- index.js
+++ index.js
@@ -36,12 +36,12 @@ const startServer = async () => {
         console.log('Database connected && server started');
 
         // use to creat db if there isn't
-        // await sequelize.sync({ alter: true }); 
-        // console.log('Tables created successfully');
+        await sequelize.sync({ alter: true }); 
+        console.log('Tables created successfully');
         
         // use to clear db
-        await sequelize.sync({ force: true }); 
-        console.log('All tables re-created successfully');
+        // await sequelize.sync({ force: true }); 
+        // console.log('All tables re-created successfully');
 
         app.listen(process.env.PORT || 5000, () => {
             console.log(`server listen on port ${process.env.PORT}`);
