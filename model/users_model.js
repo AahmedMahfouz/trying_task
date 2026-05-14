dfae3d2869ee2883ab6a4246025fda7434f7a256
Ahmed Mahfouz <ahmedmahfouz3032004@gmail.com>
2026-05-14 20:10:26 +0300

    Final fix: ensure sync and root route

--- model/users_model.js
+++ model/users_model.js
@@ -10,7 +10,7 @@ const User = sequelize.define('users', {
     fullname: {
         type: DataTypes.STRING,
         allowNull: false,
-        filed:'username'
+        field:'username'
     },
     email: {
         type: DataTypes.STRING,
