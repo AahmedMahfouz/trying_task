dac33a73d1d59e7dad530f61745d298487be6297
Ahmed Mahfouz <ahmedmahfouz3032004@gmail.com>
2026-06-11 05:24:47 +0300

    Final fix: ensure sync and root route

--- controulers/user_controulers/resetPassword.js
+++ controulers/user_controulers/resetPassword.js
@@ -1,36 +1,24 @@
 const { User, VerificationCode } = require('../../model/model_define'); 
 const appError = require('../../helpers/appError');
 const httpStatusText = require('../../helpers/httpstatustext');
-const bcrypt = require('bcryptjs');
+const bcrypt=require('bcryptjs')
 const asyncWrapper = require('../../midelware/asyncWrapper');
 const { Op } = require('sequelize');
 
 const resetPassword = asyncWrapper(async (req, res, next) => {
-    const { email, code, newPassword, confirmPassword } = req.body;
+    const {email, code, newPassword, confirmPassword } = req.body;
     
     if (!email || !code || !newPassword || !confirmPassword) {
         return next(appError.create("All fields are required", 400, httpStatusText.fail));
     }
 
-    const pass = newPassword.trim();
-    const confirmPass = confirmPassword.trim();
-
-    if (pass !== confirmPass) {
+    if (newPassword !== confirmPassword) {
         return next(appError.create("Passwords do not match", 400, httpStatusText.fail));
-    }
-
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,}$/;
-    if (!passwordRegex.test(pass)) {
-        return next(appError.create(
-            "Password must be at least 8 characters long, include uppercase and lowercase letters, a number, and a special character", 
-            400, 
-            httpStatusText.fail
-        ));
     }
 
     const user = await User.findOne({ where: { email } });
     if (!user) {
-        return next(appError.create("Data is not correct", 404, httpStatusText.fail));
+        return next(appError.create("Date is not correct", 404, httpStatusText.fail));
     }
     
     const validCode = await VerificationCode.findOne({
@@ -46,7 +34,7 @@ const resetPassword = asyncWrapper(async (req, res, next) => {
         return next(appError.create("Invalid or expired code", 400, httpStatusText.fail));
     }
 
-    const hashedPassword = await bcrypt.hash(pass, 10);
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
     user.password = hashedPassword;
     await user.save();
 
@@ -63,4 +51,4 @@ const resetPassword = asyncWrapper(async (req, res, next) => {
     res.status(200).json({ status: httpStatusText.success, msg: "Password updated successfully" });
 });
 
-module.exports = resetPassword;
+module.exports=resetPassword
